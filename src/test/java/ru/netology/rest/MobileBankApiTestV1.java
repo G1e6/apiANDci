bf824d434a8824b1166561cf85a9ae20e5c869a3
G1e6 <gleb-potapov@rambler.ru>
2022-08-15 20:35:45 +0500

    test object

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -51,6 +51,7 @@ class MobileBankApiTestV1 {
                 .then()
                 .body("[0].currency", equalTo("RUB"))
                 .body("[1].currency", equalTo("USD"))
-                .body("[0].currency", equalTo("RUR"));
+               // .body("[0].currency", equalTo("RUR"))
+                .body("[1].currency", equalTo("UST"));
     }
 }
